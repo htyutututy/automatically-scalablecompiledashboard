6122f01077a1e80a568c243d0e4cad52a8bb2a8e
htyutututy <msjcariadziobek@hotmail.com>
2024-05-14 05:49:33 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
+}
